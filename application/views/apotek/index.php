438853b85a9879f88d813a01626aa2ccb628a931
faradellawidya <faradellawidya@gmail.com>
2018-05-01 16:03:18 +0700

    update index.php

--- application/views/apotek/index.php
+++ application/views/apotek/index.php
@@ -1,9 +1,9 @@
 <?php $this->load->view('layouts/base_start') ?>
+
   <div class="jumbotron">
     <div class="container">
     <center><img class="img-responsive" src="assets/image/logo2.png" alt="" height="50px"></center>
     </div>
   </div>
-  
   
 <?php $this->load->view('layouts/base_end') ?>
