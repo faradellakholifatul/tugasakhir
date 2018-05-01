5e1350b26dfb7615eb8028801d519c070ee04077
kholifatulmahmudah <kholifatul.viiie14@gmail.com>
2018-05-01 14:20:42 +0700

    progress pertama

---
+++ application/views/layouts/base_start.php
@@ -0,0 +1,38 @@
+<!-- base_start.php -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php isset($title) ?: $title = 'Healthy Pharma'; echo $title ?></title>
+        <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+        
+        <style>
+            .navbar-brand{
+                font-family: impact;
+                font-size: 20pt;
+            }
+            .jumbotron {
+            position: relative;
+            background: #333333;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            overflow: hidden;
+            }
+            .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            background-color: #ffffff;
+            }
+
+            .container .text-footer {
+            margin: 20px 0;
+            }
+        </style>
+    </head>
+    <body>
+    <?php $this->load->view('layouts/header') ?>
---
+++ application/views/layouts/header.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#page-top">Healthy Pharma</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href=#>HOME</a></li>
+                <li><a href=#>PROFIL</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">PRODUK<b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li><a href=#>HUBUNGI KAMI</a></li>
+                <li><a href=#><img class="image" border="0" src="assets/image/troli.png" width="17px" height="15px">TROLI</a></li>
+                <li><a href=#><img class="image" border="0" src="assets/image/login.png" width="12px" height="13px">LOGIN</a></li>
+            </ul>
+        </div>
+</nav>
