641ada5dffce426ef7df4252cbeac74d69adca33
faradellawidya <38043203+faradellawidya@users.noreply.github.com>
2018-05-18 16:38:41 +0700

    Update base_end.php

--- application/views/layouts/base_end.php
+++ application/views/layouts/base_end.php
@@ -1,5 +1,45 @@
-<!-- base_end.php -->
-<?php 
-    $this->load->view('layouts/footer') ?>
-    </body>
-</html>
+<!-- base_start.php -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php isset($title) ?: $title = 'Healthy Pharma'; echo $title ?></title>
+        <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+        
+        <style>
+            .container .row {
+                position: absolute;
+                top: 20%;
+                left: 35%;
+            }
+            .navbar-brand{
+                font-family: impact;
+                font-size: 20pt;
+            }
+            .jumbotron {
+            position: relative;
+            background: #333333;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            overflow: hidden;
+            }
+            .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            background-color: #ffffff;
+            }
+
+            .container .text-footer {
+            margin: 20px 0;
+            }
+        </style>
+        <script src="<?php echo base_url('assets/js/jquery-3.3.1.min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+    </head>
+    <body>
+    <?php $this->load->view('layouts/header') ?>
