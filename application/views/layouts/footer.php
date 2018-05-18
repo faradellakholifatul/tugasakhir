a3880d36f362c33b63638dc4a13b551ed0187b4a
kholifatulmahmudah <kholifatul.viiie14@gmail.com>
2018-05-18 15:42:28 +0700

    Modifikasi database.php

--- application/views/layouts/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- footer.php -->
-<footer class="footer">
-    <div class="container">
-        <p class="text-footer">
-            <center>Hak Cipta <a href="https://github.com/faradellakholifatul">Healthy Pharma</a></center>
-        </p>
-    </div>
-</footer>
