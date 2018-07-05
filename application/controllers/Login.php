e2dec41c79405e7cff38489a7b4fe8184a20965b
faradellawidya <38043203+faradellawidya@users.noreply.github.com>
2018-07-05 15:18:16 +0700

    Modifikasi controller Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,15 +6,52 @@ class Login extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url');
+        $this->load->library('session');
+        
+        $this->load->helper('url', 'form');
+        $this->load->model('Login_model');
     }
     public function index()
     {
         $this->load->view('apotek/login');
     }
 
+    public function login()
+    {
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $set = $this->Login_model->login($username, $password);
+        if($set)
+        { 
+            $log = [
+                'id' => $set->id,
+                'username' => $set->username,
+                'privilege' => $set->privilege,
+                'status' => 'Logged'
+            ];
+            $this->session->set_userdata($log);            
+
+            if($this->Login_model->login($username, $password))
+            {
+                if($this->session->privilege=="Administrator")
+                { redirect('admin/'); }
+                else
+                { redirect('apotek/'); }
+            }
+            else
+            {
+                echo 12345;
+                //redirect('apotek/login');
+            }
+        }
+    }
+
+    public function logout()
+    { 
+        $this->session->sess_destroy();
+        redirect('home');
+    }
 }
 
 /* End of file Login.php */
-
 ?>
