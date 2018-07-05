97897cddde27d30bfa146b7433b664c940f11566
kholifatulmahmudah <38054784+kholifatulmahmudah@users.noreply.github.com>
2018-07-05 16:29:31 +0700

    Upload controller user

---
+++ application/controllers/User.php
@@ -0,0 +1,28 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class User extends CI_Controller {
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->helper('url', 'form');
+            $this->load->model('Login_model');
+        }
+
+        public function index()
+        {
+            $session_id = $this->session->userdata('user');
+            if(!empty($session_id))
+            {
+                redirect('apotek/');
+            }
+
+            $data['user'] = $session_id;
+            $this->load->view('apotek', $data);
+        }
+    
+    }
+    
+    /* End of file .php */
+    
