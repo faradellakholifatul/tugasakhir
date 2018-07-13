bae387636cd81ea23926084883075acb84ee04b0
faradellawidya <faradellawidya@gmail.com>
2018-07-13 08:16:14 +0700

    upload controller

---
+++ application/models/Login_model.php
@@ -0,0 +1,19 @@
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Login_model extends CI_Model {
+    
+        public function login($username, $password)
+        {
+            $this->db->select('*');
+            $this->db->from('user');
+            $this->db->where('username', $username);
+            $this->db->where('password', $password);
+            return $this->db->get()->row();
+        }
+    
+    }
+    
+    /* End of file Login_model.php */
+    
