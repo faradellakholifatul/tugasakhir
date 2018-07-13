181d446059d699254cd96d40e7d16513404f9d14
faradellawidya <faradellawidya@gmail.com>
2018-07-13 08:15:21 +0700

    upload layout footer

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -2,7 +2,7 @@
 <footer class="footer">
     <div class="container">
         <p class="text-footer">
-            <center>Hak Cipta <a href="https://github.com/faradellakholifatul">Healthy Pharma</a></center>
+            <center>Hak Cipta<a href="https://github.com/faradellakholifatul">Healthy Pharma</a></center>
         </p>
     </div>
 </footer>
