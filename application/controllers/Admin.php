b4787e186e38cc152df896f5c29b9966c8df5996
faradellawidya <faradellawidya@gmail.com>
2018-07-13 08:16:34 +0700

    modifikasi controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
     
     defined('BASEPATH') OR exit('No direct script access allowed');
@@ -21,4 +22,29 @@
     }
     
     /* End of file Admin.php */
+=======
+<?php
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Admin extends CI_Controller {
+    
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->library('session');
+            if($this->session->privilege!='Administrator')
+            { 
+               redirect('login/'); 
+            }
+        }
+        public function index()
+        {
+            $this->load->view('admin/index');
+        }
+    
+    }
+    
+    /* End of file Admin.php */
+>>>>>>> 0f475ee15e1c9ace9191a4b241aefb2ed68d437f
     
