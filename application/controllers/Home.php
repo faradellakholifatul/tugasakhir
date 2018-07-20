b7718450563638097f124cd45fc87332c1cc4a4b
kholifatulmahmudah <kholifatul.viiie14@gmail.com>
2018-07-20 18:54:42 +0700

    modifikasi controller home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,11 +7,19 @@
         {
             parent::__construct();
             $this->load->helper('url');
+            $this->load->model('Produk_model');
         }
 
         public function index()
         {
-            $this->load->view('apotek/index');
+            $data['list'] = $this->Produk_model->list();
+            $this->load->view('apotek/index', $data);
+        }
+
+        public function etical()
+        {
+            $data['getEtical'] = $this->Produk_model->getEtical();
+            $this->load->view('apotek/etical', $data);
         }
 
         public function register()
@@ -40,6 +48,11 @@
         {
             $this->load->view('apotek/vismis');
             
+        }
+
+        public function detail()
+        {
+            $this->load->view('apotek/detail');
         }
     
     }
